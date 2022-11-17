f617d3f34b07f4d9a9fd8e0c1be4c30747317d6b
mohammadeshaghi <mohammadeshaghi1998@gmail.com>
2022-11-17 21:50:02 +0330

    feat: install and config swagger

---
+++ app/Swagger/info.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="L5 Swagger OpenApi description"
+ * )
+ *
+ */
+
+/**
+ *  @OA\Server(
+ *      url="/api/",
+ *      description=""
+ *  )
+ */
+
+/**
+ * @OA\SecurityScheme(
+ *      securityScheme="bearerAuth",
+ *      in="header",
+ *      name="bearerAuth",
+ *      type="http",
+ *      scheme="bearer",
+ *      bearerFormat="JWT",
+ * )
+ */
+
+/**
+ * @OA\Response(
+ *   response="200",
+ *   description="OK",
+ *   @OA\JsonContent({ @OA\Schema(ref="#/components/schemas/SuccessResult")}))
+ */
+
+/**
+ * @OA\Response(
+ *   response="422",
+ *   description="The given data is invalid",
+ *     @OA\JsonContent(oneOf={ @OA\Schema(ref="#/components/schemas/ErrorResult")}))
+ */
+
+/**
+ * @OA\Response(
+ *   response="401",
+ *   description="Unauthenticate",
+ *     @OA\JsonContent(oneOf={ @OA\Schema(ref="#/components/schemas/ErrorResult")}))
+ */
+
+/**
+ * @OA\Response(
+ *   response="403",
+ *   description="Unauthorize",
+ *     @OA\JsonContent(oneOf={ @OA\Schema(ref="#/components/schemas/ErrorResult")}))
+ */
+
+/**
+ * @OA\Schema(
+ *  schema="ErrorResult",
+ *  title="Error Result",
+ * 	@OA\Property(
+ *        property="message",
+ *        type="string"
+ *    ),
+ * 	@OA\Property(
+ *        property="errors",
+ *        type="array",@OA\Items(type="string"),
+ *    )
+ * )
+ */
+
+/**
+ * @OA\Schema(
+ *  schema="SuccessResult",
+ *  title="Success Result"
+ * )
+ */
